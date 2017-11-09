c1d61fb30bdf26054ea0cae9c608ba894cab7758
Basawaraj Savalagi <asbasawaraj@gmail.com>
2017-11-09 12:07:49 +0530

    changes to print formats

---
+++ selco/selco/doctype/material_request_type/material_request_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, SELCO and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Material Request Type', {
+	refresh: function(frm) {
+
+	}
+});
