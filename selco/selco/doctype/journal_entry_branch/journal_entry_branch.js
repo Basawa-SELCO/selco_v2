eba1ef93fe6d8fb7288e5b021099aa123203487e
Basawaraj <asbasawaraj@gmail.com>
2017-12-18 12:11:07 +0530

    changes to custom

---
+++ selco/selco/doctype/journal_entry_branch/journal_entry_branch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, SELCO and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Journal Entry Branch', {
+	refresh: function(frm) {
+
+	}
+});
