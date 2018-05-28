6c4eaa0eab4b922e8463f00aef495ebb5dcc381a
Siddhi Sawant <siddhisawant8@gmail.com>
2018-05-28 19:53:20 +0530

    Added .js file for filter in Pending Rejection In From Supplier Report

---
+++ selco/selco/report/pending_rejection_in_from_supplier/pending_rejection_in_from_supplier.js
@@ -0,0 +1,11 @@
+frappe.query_reports["Pending Rejection In From Supplier"] = {
+	"filters": [
+		{
+				"fieldname":"selco_branch",
+				"label": __("Branch"),
+				"fieldtype": "Link",
+				"options": "Branch"
+		}
+
+	]
+}
