9b12f6114e6994d5d827b154fa88c0ff24b8570f
Siddhi Sawant <siddhisawant8@gmail.com>
2018-05-29 16:04:37 +0530

    [fix]Stock Ledger-SELCO Report

--- selco/selco/report/stock_ledger_selco/stock_ledger_selco.js
+++ selco/selco/report/stock_ledger_selco/stock_ledger_selco.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Stock Ledger - SELCO"] = {
+frappe.query_reports["Stock Ledger-SELCO"] = {
 	"filters": [
 		{
 			"fieldname":"company",
