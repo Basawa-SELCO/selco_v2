ad837afd6d2fbef1ee07928161210d73d361111e
Gaurav Naik <gvrvnk@gmail.com>
2018-06-05 19:44:08 +0530

    [Fix] selco_account_cash in payment entry after insert and validate

--- selco/selco/selco_customizations.py
+++ selco/selco/selco_customizations.py
@@ -472,7 +472,7 @@ def selco_payment_entry_before_insert(doc,method):
             else:
                 doc.paid_to = frappe.db.get_value("Branch",doc.selco_branch,"selco_collection_account")
         elif doc.mode_of_payment == "Cash":
-            doc.paid_to = frappe.db.get_value("Branch",doc.selco_branch,"selco_collection_account_cash")
+            doc.paid_to = frappe.db.get_value("Branch",doc.selco_branch,"selco_collection_account")
     elif doc.payment_type == "Pay":
         if doc.mode_of_payment == "Bank":
             doc.naming_series = frappe.db.get_value("Branch",doc.selco_branch,"selco_bank_payment_naming_series")
@@ -482,7 +482,7 @@ def selco_payment_entry_validate(doc,method):
     if doc.payment_type == "Receive":
         if doc.selco_money_received_by == "Cash":
             doc.mode_of_payment = "Cash"
-            doc.paid_to = frappe.db.get_value("Branch",doc.selco_branch,"selco_collection_account_cash")
+            doc.paid_to = frappe.db.get_value("Branch",doc.selco_branch,"selco_collection_account")
         else:
             doc.mode_of_payment = "Bank"
             doc.paid_to = frappe.db.get_value("Branch",doc.selco_branch,"selco_collection_account")
