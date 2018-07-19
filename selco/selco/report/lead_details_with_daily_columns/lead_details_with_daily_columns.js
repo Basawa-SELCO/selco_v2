0414e2c872330ec461175e4a8627c38f809d24ae
Gaurav Naik <gvrvnk@gmail.com>
2018-07-19 11:22:16 +0530

    [WIP] Lead Details with Daily Columns

---
+++ selco/selco/report/lead_details_with_daily_columns/lead_details_with_daily_columns.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, SELCO and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Lead Details with Daily Columns"] = {
+	"filters": [
+
+	]
+}
