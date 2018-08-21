f2120f3e630c9a2757d5ec57269ade5cd98c9faa
Rohit Waghchaure <rohitw1991@gmail.com>
2018-08-21 16:27:04 +0530

    Added new condition in the selco_stock_entry_on_cancel_updates method

--- selco/selco/selco_customizations.py
+++ selco/selco/selco_customizations.py
@@ -706,11 +706,12 @@ def selco_stock_entry_on_cancel_updates(doc,method):
     if(doc.selco_type_of_stock_entry == "GRN"):
         for item in doc.items:
             if item.reference_rej_in_or_rej_ot:
-                ref_doc = frappe.get_doc("Stock Entry",item.reference_rej_in_or_rej_ot)
-                for ref_item in ref_doc.items:
-                    if ref_item.item_code == item.item_code:
-                        ref_item.reference_rej_in_or_rej_quantity = cint(ref_item.reference_rej_in_or_rej_quantity) - cint(item.qty)
-                ref_doc.save()
+                if doc.selco_type_of_material == "Repair Stock":
+                    ref_doc = frappe.get_doc("Stock Entry",item.reference_rej_in_or_rej_ot)
+                    for ref_item in ref_doc.items:
+                        if ref_item.item_code == item.item_code:
+                            ref_item.reference_rej_in_or_rej_quantity = cint(ref_item.reference_rej_in_or_rej_quantity) - cint(item.qty)
+                    ref_doc.save()
 
 """@frappe.whitelist()
 def cleanup_si():
