e256a3f07da0620d911524111542fcc32d3583e3
Your Name <you@example.com>
2020-10-10 18:23:03 +0200

    added custom doctypes

---
+++ selco/selco/doctype/selco_events/selco_events.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, SELCO and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SELCO Events', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ selco/selco/doctype/service_record/service_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, SELCO and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service Record', {
+	// refresh: function(frm) {
+
+	// }
+});
